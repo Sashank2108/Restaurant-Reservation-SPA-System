273e6f43753e40e2cb6f0729e6579384b95691f9
Sashank2108 <152713153+Sashank2108@users.noreply.github.com>
2025-10-28 19:58:42 +0530

    Add files via upload

---
+++ server/routes/reservationRoute.js
@@ -0,0 +1,9 @@
+import Router from 'express';
+import {sendReservation} from '../controller/reservation.js'
+
+const router = Router();
+
+router.post('/send', sendReservation);
+
+
+export default router;
